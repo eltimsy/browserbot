24fbdab2a14d70f9145fc4978a73a21c9d8019c5
eltimsy <timothy.sc.tse@gmail.com>
2017-07-19 14:18:19 -0400

    added some fun stuff to make it click more things

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const Nightmare = require('nightmare');
-const nightmare = Nightmare({ show: true});
+const nightmare = Nightmare({ show: true, switches: {'ignore-certificate-errors': true}});
+
+Nightmare.action('clickAll', function(selector, done) {
+  this.evaluate_now((selector)=> {
+    var elements = document.querySelectorAll(selector);
+    elements.forEach((element) => {
+      var event = document.createEvent('MouseEvent');
+      event.initEvent('click', true, true);
+      element.dispatchEvent(event);
+    })
+  }, done, selector)
+})
+
 
 nightmare
-  .goto('https://ratemds.com')
-  .type('#doctor-search', 'super')
+  .goto('')
+  .type('#doctor-search', 'abc')
   .click('.input-group-btn button')
-  .wait('.hd .r')
-  .evaluate(function () {
-    return document.querySelector('.hd .r a').href;
-  })
-  .end()
+  .wait('#doctor-list')
+  .click('#doctor-list .search-item-doctor-link')
+  .wait('#doctor-rating-form')
+  .clickAll('#doctor-rating-form .stars .star')
+  .type('#doctor-rating-form .form-control', 'I am a super bot and I can write ratings by myself')
+  .click('#doctor-rating-form button')
+  // .evaluate(function () {
+  //   return document.querySelector('#doctor-list .search-item-doctor-name').textContent;
+  // })
+  // .end()
   .then(function (result) {
     console.log(result);
   })
