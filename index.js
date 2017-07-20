e3a0067544dcdfaf20f0244a3884cce873498e6d
eltimsy <timothy.sc.tse@gmail.com>
2017-07-20 14:48:54 -0400

    made it so bot can run in a loop

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
-const Nightmare = require('nightmare');
-const nightmare = Nightmare({ show: true, switches: {'ignore-certificate-errors': true}});
+// frameDocument = document.querySelector('frame').contentDocument
+// otherFrame = frameDocument.querySelector('otherframe').contentDocument
+// button = otherFrame.querySelector('#submit')
+// button.click()
+(function RunNightmare(i) {
+  const promise = new Promise((resolve, reject) => {
+    const Nightmare = require('nightmare');
+    const nightmare = Nightmare({ show: true, switches: {'ignore-certificate-errors': true}, webSecurity: false, alwaysOnTop: false});
 
-Nightmare.action('clickAll', function(selector, done) {
-  this.evaluate_now((selector)=> {
-    var elements = document.querySelectorAll(selector);
-    elements.forEach((element) => {
-      var event = document.createEvent('MouseEvent');
-      event.initEvent('click', true, true);
-      element.dispatchEvent(event);
+    Nightmare.action('clickAll', function(selector, done) {
+      this.evaluate_now((selector)=> {
+        var elements = document.querySelectorAll(selector);
+        elements.forEach((element) => {
+          var event = document.createEvent('MouseEvent');
+          event.initEvent('click', true, true);
+          element.dispatchEvent(event);
+        })
+      }, done, selector)
     })
-  }, done, selector)
-})
+
+    nightmare
+      .goto('')
+      .type('#doctor-search', doctors[i])
+      .click('.input-group-btn button')
+      .wait('#doctor-list')
+      .click('#doctor-list .search-item-doctor-link')
+      .wait('#doctor-rating-form')
+      .clickAll('#doctor-rating-form .stars .star')
+      .type('#doctor-rating-form .form-control', 'I am a super bot and I can write ratings by myself')
+      .click('#doctor-rating-form button')
+      .wait(1000)
+      // .evaluate(function () {
+      //   return document.querySelector('#doctor-list .search-item-doctor-name').textContent;
+      // })
+      .end()
+      .then(function (result) {
+        console.log(i)
+        resolve();
+      })
+      .catch(function (error) {
+        console.log('failed')
+        reject();
+      });
+  }).then( () => i >= 5 || RunNightmare(i+1))
+  .catch(() => i >= 5 || RunNightmare(i+1));
+})(0);
 
 
-nightmare
-  .goto('')
-  .type('#doctor-search', 'abc')
-  .click('.input-group-btn button')
-  .wait('#doctor-list')
-  .click('#doctor-list .search-item-doctor-link')
-  .wait('#doctor-rating-form')
-  .clickAll('#doctor-rating-form .stars .star')
-  .type('#doctor-rating-form .form-control', 'I am a super bot and I can write ratings by myself')
-  .click('#doctor-rating-form button')
-  // .evaluate(function () {
-  //   return document.querySelector('#doctor-list .search-item-doctor-name').textContent;
-  // })
-  // .end()
-  .then(function (result) {
-    console.log(result);
-  })
-  .catch(function (error) {
-    console.error('Search failed:', error);
-  });
+
